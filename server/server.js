5e3f3cf3fc2579f77441571c1dc46ed3bea86cba
Daniel Fearn <dlhfearn@gmail.com>
2021-07-22 17:40:38 +0100

    tidied endpoints

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const app = require('express')();
 const controller = require('./sequenceController')(false);
+const path = require('path');
 
 
 app.get('/upload/:data', controller.uploadHandler);
 
 app.get('/:command', controller.commandHandler);
+
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, '../client/gui.html'));});
 
 app.listen(80);
