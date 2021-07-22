1ddf2fc85f6e0389eb55f12f51b7d2389824d4db
Daniel Fearn <dlhfearn@gmail.com>
2021-07-22 15:59:58 +0100

    added endpoints to match existing frontend

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const app = require('express')();
+
+
+app.get('/upload/:data', (req, res) => {});
+
+app.get('/faceall', (req, res) => {});
+app.get('/awayall', (req, res) => {});
+app.get('/start', (req, res) => {});
+app.get('/stop', (req, res) => {});
+
+app.listen('App started on port 80');
