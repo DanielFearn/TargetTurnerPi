1dd87b8b9c176a7aecf66201eb1a6ac2a97b2db4
Daniel Fearn <dlhfearn@gmail.com>
2021-07-22 19:09:43 +0100

    implemented faceall and awayall commands

--- server/sequenceController.js
+++ server/sequenceController.js
@@ -1,5 +1,13 @@
 module.exports = function(debugging = false) {
 
+    var targetStates = [];
+    targetStates.length = 10;
+    targetStates.fill(0);
+
+    function printTargets(){
+        if(!debugging) return;
+        console.log('State: '+targetStates.join(''));
+    }
 
     return {
         uploadHandler: (req, res) => {
@@ -9,6 +17,18 @@ module.exports = function(debugging = false) {
 
         commandHandler: (req, res) => {
             console.log('Command: '+req.params.command);
+
+            switch (req.params.command){
+                case 'faceall':
+                    targetStates.fill(1);
+                    break;
+                case 'awayall':
+                    targetStates.fill(0);
+                    break;
+            }
+
+            printTargets();
+
             res.end('OK');
         }
     }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const controller = require('./sequenceController')(false);
+const controller = require('./sequenceController')(true);
 const path = require('path');
 
 
